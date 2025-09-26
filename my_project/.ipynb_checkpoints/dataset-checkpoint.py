e904d6e270995d89cce1a87e57db03d0457c3133
lalana.146622 <lalana.146622@e.unavarra.es>
2025-09-26 22:03:00 +0200

    Added visualization notebooks and refractor train.py

---
+++ my_project/.ipynb_checkpoints/dataset-checkpoint.py
@@ -0,0 +1,82 @@
+import pandas as pd
+import sys
+import os
+import shutil
+import zipfile
+import cv2
+import numpy as np
+import torch
+from torchvision import datasets, transforms
+from torch.utils.data import random_split, DataLoader
+from PIL import Image
+
+
+def load_data_from_zip(output_dir):
+    with zipfile.ZipFile(output_dir+'.zip', 'r') as zipf:
+            zipf.extractall(output_dir)
+
+def read_dataset(dataset_dir):
+
+    if not os.path.exists(dataset_dir):
+        print("No existe el directorio con las imagenes:", dataset_dir)
+        load_data_from_zip(dataset_dir)
+    
+
+    df = pd.read_csv(os.path.join(dataset_dir + ".csv"))
+    X = []
+    y = []
+
+    transform = transforms.Compose([
+        transforms.Resize((256, 256)), 
+        transforms.ToTensor(),
+        transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5))
+    ])
+    
+    for _, row in df.iterrows():
+        img = os.path.join(dataset_dir, row['file_name'].replace('train_data/', ''))
+        if img is not None:
+            img_cv = cv2.imread(img)       
+            img_cv = cv2.cvtColor(img_cv, cv2.COLOR_BGR2RGB)
+
+            img_pil = Image.fromarray(img_cv)
+
+            img_tensor = transform(img_pil)
+
+            X.append(img_tensor)
+            y.append(row['label'])
+            
+    X = torch.stack(X)
+    y = torch.tensor(y, dtype=torch.long)
+    return X, y
+
+
+if __name__ == "__main__":
+
+    output_dir = "data/interim/initial_data"
+    if os.path.exists(output_dir + ".zip"):
+        load_data_from_zip(output_dir)
+
+    else:
+        data = pd.read_csv("data/raw/train.csv")
+
+        initial_data = data[:1000]
+        initial_data.to_csv("data/interim/initial_data.csv", index=False)
+
+        for picture in initial_data['file_name']:
+            output_dir = "data/interim/initial_data"
+            os.makedirs(output_dir, exist_ok=True)
+            src_path = os.path.join("data/raw/", picture)
+            dst_path = os.path.join(output_dir, picture.replace('train_data/', ''))
+            shutil.copy2(src_path, dst_path)
+
+        zip_path = output_dir + ".zip"
+        with zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+            for root, _, files in os.walk(output_dir):
+                for file in files:
+                    file_path = os.path.join(root, file)
+                    arcname = os.path.relpath(file_path, output_dir)
+                    zipf.write(file_path, arcname)
+
+
+
+
